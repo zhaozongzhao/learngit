960ad0cdcda2678de511dd8811bc6776f07e6e0e
zzz <2206321864@qq.com>
2018-01-29 00:31:49 +0800

    日志分天显示

--- dentistry/dental/case/login.py
+++ dentistry/dental/case/login.py
@@ -30,11 +30,14 @@ class operation():
   #读取文件
   def read_csv(self):
     '''读取文件'''
-    filename =time.strftime('%Y-%m-%d',time.localtime(time.time()))+ '.csv'
-    file_name = self.get_current_path(filename)
-    dats = csv.reader(open(file_name,mode='r'))
-    for user in dats:
+    try:
+      filename =time.strftime('%Y-%m-%d',time.localtime(time.time()))+ '.csv'
+      file_name = self.get_current_path(filename)
+      dats = csv.reader(open(file_name,mode='r'))
+      for user in dats:
         return user
+    except Exception as a:
+        return False
 
   #csv文件写入
   def writr_csv_file(self,list_data):
@@ -95,9 +98,9 @@ class Browser(operation):
              payload={"username":"channel","password":password,'mobile':int(i),"content":content,
                    'productid':95533,'xh':0}
              logging.critical(self.read_time()+'发送短信内容'+str(payload))
-             result = requests.post(url,data=payload)
-             print(result.text)
-             logging.critical(self.read_time()+'返回的的发送信息'+str(result.text))
+             # result = requests.post(url,data=payload)
+             # print(result.text)
+             # logging.critical(self.read_time()+'返回的的发送信息'+str(result.text))
    except Exception as errormessage:
       print(errormessage)
 
@@ -110,7 +113,7 @@ class Browser(operation):
          self.driver.get('https://taiyiyun.taikang.com/saas/view/saasLogin.html')#正式地址
          self.driver.find_element_by_xpath('//*[@id="loginform"]/div[1]/div/div/input').send_keys('bbykyzkj0')
          self.driver.find_element_by_xpath('//*[@id="loginform"]/div[2]/div/div/input').send_keys('bbykyzkj1234')
-         self.driver.find_element_by_xpath('//*[@id="checkBtn"]').click()
+         self.driver.find_element_by_xpath('/''/*[@id="checkBtn"]').click()
       except Exception as a:
           print('登录错误',a)
 
@@ -146,15 +149,16 @@ class Browser(operation):
   def is_phone(self,phone):
      '''判断手机号码'''
      old_phone =self.read_csv()[3]
-     logging.warning(self.read_time()+'读取的手机号码'+old_phone)
-     new_phone = phone
-     logging.warning(self.read_time()+'当前手机号码的手机号码'+new_phone)
-     print('************************')
-     if old_phone == new_phone:
-         logging.info(self.read_time()+'当前没有新的患者信息')
-     elif old_phone == '' or new_phone == '':
-         logging.error(self.read_time()+'手机号码错误')
-     else:
+     if old_phone:
+        logging.warning(self.read_time()+'读取的手机号码'+old_phone)
+        new_phone = phone
+        logging.warning(self.read_time()+'当前手机号码的手机号码'+new_phone)
+        print('************************')
+        if old_phone == new_phone:
+            logging.info(self.read_time()+'当前没有新的患者信息')
+        elif old_phone == '' or new_phone == '':
+               logging.error(self.read_time()+'手机号码错误')
+        else:
          self.driver.refresh()
          logging.critical(self.read_time()+'发送短信')
          #发短信操作
@@ -168,10 +172,14 @@ class Browser(operation):
          #将新的患者数据写入表格
          self.delete_file()
          self.list_data_get()
+     else:
+         self.list_data_get()
 
   #定时任务
   def printtime(self):
-    logging.basicConfig(filename='example.log', level=logging.DEBUG)
+    filename=time.strftime('%Y-%m-%d',time.localtime(time.time()))+ '.log'
+    file_name = self.get_current_path(filename)
+    logging.basicConfig(filename=file_name, level=logging.DEBUG)
     while True:
         new_phone = self.get_table_phone()
         self.is_phone(new_phone)
